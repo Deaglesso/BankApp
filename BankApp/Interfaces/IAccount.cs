ecb5d4b25e32ccfb588f3ff0d150537592882499
Deaglesso <seid.beam@gmail.com>
2023-10-21 16:16:43 +0400

    Created Intefaces

---
+++ BankApp/Interfaces/IAccount.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankApp.Interfaces
+{
+    internal interface IAccount
+    {
+        string AccountID { get; }
+        decimal Balance { get; }
+
+        
+        public void Deposit(decimal amount);
+        public void Withdraw(decimal amount);
+
+    }
+}
