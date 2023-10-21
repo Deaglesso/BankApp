3556724f2c9c02221bb438ff837cd0a93a9970a1
Deaglesso <seid.beam@gmail.com>
2023-10-21 22:29:51 +0400

    Created Accounts

---
+++ BankApp/Models/Account.cs
@@ -0,0 +1,28 @@
+﻿using BankApp.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankApp.Models
+{
+    internal class Account : IAccount
+    {
+        public string AccountID;
+
+        public decimal Balance;
+
+        private List<Transaction> Transactions { get; set; }
+
+        public void Deposit(decimal amount)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Withdraw(decimal amount)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
